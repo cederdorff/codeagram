5e75bb100a67a68d04eac578bef378b34f3422f3
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2026-05-07 11:33:47 +0200

    refactor: simplify UserInfo component structure and update CSS class references

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -2,10 +2,8 @@ export function UserInfo({ avatar, timestamp, username }) {
   return (
     <div className="user-info">
       <img className="avatar" src={avatar} />
-      <div className="user-copy">
-        <h2>{username}</h2>
-        <p>{timestamp}</p>
-      </div>
+      <h2>{username}</h2>
+      <p>{timestamp}</p>
     </div>
   );
 }
